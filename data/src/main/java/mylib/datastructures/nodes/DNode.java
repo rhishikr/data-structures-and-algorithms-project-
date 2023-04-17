3e863095bbe510a53c8be69ecd8f2d15b180f22f
rhishikr <rhishik.royninabh@ucalgary.ca>
2023-04-17 09:09:09 -0600

    added DNode

---
+++ data/src/main/java/mylib/datastructures/nodes/DNode.java
@@ -0,0 +1,65 @@
+package mylib.datastructures.nodes;
+
+public class DNode {
+
+    private Integer value;
+
+    private DNode next;
+
+    private DNode prev;
+
+    public DNode(int value) {
+
+        this.value = value;
+
+        this.next = null;
+
+        this.prev = null;
+
+    }
+
+    public Integer getValue() {
+
+        return value;
+
+    }
+
+    public void setValue(Integer value) {
+
+        this.value = value;
+
+    }
+
+    public DNode getNext() {
+
+        return next;
+
+    }
+
+    public void setNext(DNode next) {
+
+        this.next = next;
+
+    }
+
+
+    public DNode getPrev() {
+
+        return prev;
+
+    }
+
+
+    public void setPrev(DNode prev) {
+
+        this.prev = prev;
+
+    }
+
+    public String toString() {
+
+        return value.toString();
+
+    }
+
+}
